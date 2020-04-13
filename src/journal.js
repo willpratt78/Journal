df2a5432e0d93862d0596dbb63a63816941d71d4
William Pratt <willpratt78@gmail.com>
2020-04-13 16:29:34 -0700

    move UI logic into new format specs

--- src/journal.js
+++ src/journal.js
@@ -7,29 +7,8 @@ function BlogPosts() {
   this.bodies = [];
 }
 
-this.titles.push("#title")
-this.bodies.push("#body")
+BlogPosts.prototype.addEntry = function(entry) {
+  this.titles.push(entry);
 
-
-}
-
-
-// UI Logic (main.js)
-
-$(document).ready(function() {
-  $("form#journal-form").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedTitles = $("#title input['text]").val();
-    var inputtedBody = $("#body input['text']").val();
-    var 
-  
-  });
-});
-
-
-// output
-
-
-
+};
 
--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@
 
 import { journal } from './journal';
 
+$(document).ready(function() {
+  $("form#journal-form").submit(function(event) {
+    event.preventDefault();
+    var inputtedTitles = $("input#title").val();
+    var inputtedBody = $("input#body").val();
+    var newEntry = new Entry(inputtedTitles, inputtedBody); 
+
+
+    journalEntry.addEntry(newEntry);
+    
+    displayEntryDetails(journalEntry);
+
+    console.log(newEntry);
+
+   
+
+  });
+
+});
